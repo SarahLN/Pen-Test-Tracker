db0e82f0cc527418b1256ea9d8778893e5cbc50b
SarahLN <noles.sarah@gmail.com>
2018-10-05 17:17:19 -0500

    add header section

---
+++ src/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default class Header extends React.Component {
+  render() {
+    return (<div>
+        <h2>Welcome to our app!</h2>
+    </div>);
+  }
+}
