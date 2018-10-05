bd49cbb5e064e601280573d6b7a59ff206a7247e
SarahLN <noles.sarah@gmail.com>
2018-10-05 17:15:05 -0500

    rename app to toolbar

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-export default class App extends React.Component {
+export default class AppToolbar extends React.Component {
   render() {
     return (<div>
         <AppBar position="static">
