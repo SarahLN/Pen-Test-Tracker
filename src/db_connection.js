3759a975bbff380648614598571f72f3a8c6bb06
SarahLN <noles.sarah@gmail.com>
2018-10-05 18:00:42 -0500

    add db_connection

--- src/db_connection.js
+++ src/db_connection.js
@@ -1,1 +1,19 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host  : 'localhost',
+  user  : 'root',
+  password  : 'PASSWORD HERE',
+  database  : 'pentestdb'
+});
+
+connection.connect();
+
+connection.query('INSERT INTO testtable (description) VALUES ("test1")', function (error, results, fields) {
+  if (error) throw error;
+  console.log('1 + 1 = ', results[0].result);
+});
+
+connection.end();
+
 alert('test');
