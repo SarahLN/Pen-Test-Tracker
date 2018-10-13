c1da8cf29323093971f6ce6e95bb29dc61fe74f9
SarahLN <noles.sarah@gmail.com>
2018-10-13 08:42:16 -0500

    add menu icon to toolbar

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 export default class AppToolbar extends React.Component {
@@ -9,6 +11,9 @@ export default class AppToolbar extends React.Component {
     return (<div>
         <AppBar position="static">
             <Toolbar>
+              <IconButton color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
               <Typography variant="title" color="inherit">
                 Pen Test Tracker
               </Typography>
