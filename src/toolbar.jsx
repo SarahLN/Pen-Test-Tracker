0889c07142d46aef07403cb6549e23ee59977ed7
SarahLN <noles.sarah@gmail.com>
2018-10-13 08:29:49 -0500

    upgrade to react 15.6

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -3,14 +3,15 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+
 export default class AppToolbar extends React.Component {
   render() {
     return (<div>
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="title" color="inherit">
+              <Typography variant="title" color="inherit">
                 Pen Test Tracker
-                </Typography>
+              </Typography>
             </Toolbar>
         </AppBar>
     </div>);
