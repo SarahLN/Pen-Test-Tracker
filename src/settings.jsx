e0630bcb17accb4fd19120db54294da512210ee6
SarahLN <noles.sarah@gmail.com>
2018-10-13 21:33:34 -0500

    add textfield for data folder

--- src/settings.jsx
+++ src/settings.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-export default class Settings extends React.Component {
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>
-        Stuff goes here.
+        <TextField
+          required
+          id="outlined-full-width"
+          label="Data Folder"
+          style={{ margin: 8 }}
+          helperText="Specify the folder where tool output will be picked up."
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
       </div>
     )
   }
 }
+
+export default Settings;
