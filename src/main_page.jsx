add0863f8dd143595a0b9f4643b37299b5e36ecb
SarahLN <noles.sarah@gmail.com>
2018-10-15 21:41:50 -0500

    set up navigation to flow to different pages
    Outstanding issue: the menu doesn't actually close

--- src/main_page.jsx
+++ src/main_page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import AppToolbar from './components/toolbar'
+import Settings from './components/settings'
 
 class App extends React.Component {
   constructor(props) {
@@ -19,11 +20,20 @@ class App extends React.Component {
 
   render() {
     const currPage = this.state.currPage;
+    let display_page;
+    if (currPage === "Settings") {
+      // Load the settings page
+      display_page = <Settings />;
+    } else {
+      // Load the default page
+      display_page = <p>This is the main page</p>;
+    }
     return(
       <div>
         <AppToolbar
           state={this.state}
           onMenuSelect={this.handlePageChange} />
+        {display_page}
       </div>
     )
   }
