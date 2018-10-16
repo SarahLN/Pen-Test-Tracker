d0fc479ebc1c9142a439cef66c04177467c42d7a
SarahLN <noles.sarah@gmail.com>
2018-10-15 21:34:31 -0500

    add logic for handling menu click

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -4,22 +4,59 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import Settings from './settings'
 
 
 export default class AppToolbar extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: props.state.name
+      name: props.state.name,
+      anchorEl: null,
     }
   }
+
+  handleClick(event) {
+    console.log(event.currentTarget.id);
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose(event) {
+    console.log(event.currentTarget.id);
+    this.props.onMenuSelect(event.currentTarget.id);
+  }
+
   render() {
+    const { anchorEl } = this.state;
+
     return (<div>
         <AppBar position="static">
             <Toolbar>
-              <IconButton color="inherit" aria-label="Menu">
+              <IconButton
+                aria-owns={anchorEl ? 'toolbar-menu' : null}
+                aria-haspopup="true"
+                onClick={this.handleClick.bind(this)}
+                color="inherit"
+                id="Menu"
+              >
                 <MenuIcon />
               </IconButton>
+              <Menu
+                id="toolbar-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleClose.bind(this)}
+              >
+                <MenuItem
+                  id="Settings"
+                  onClick={this.handleClose.bind(this)}
+                >
+                  Settings
+                </MenuItem>
+              </Menu>
               <Typography variant="title" color="inherit">
                 Pen Test Tracker, {this.state.name}
               </Typography>
--- src/main_page.jsx
+++ src/main_page.jsx
@@ -5,16 +5,25 @@ import AppToolbar from './components/toolbar'
 class App extends React.Component {
   constructor(props) {
     super(props)
-
+    this.handlePageChange = this.handlePageChange.bind(this);
     this.state = {
-      name: "Sarah"
+      name: "Sarah",
+      currPage: null,
     }
   }
 
+  handlePageChange(newPage) {
+    console.log("new page selected " + newPage);
+    this.setState({currPage: newPage});
+  }
+
   render() {
+    const currPage = this.state.currPage;
     return(
       <div>
-        <AppToolbar state={this.state}/>
+        <AppToolbar
+          state={this.state}
+          onMenuSelect={this.handlePageChange} />
       </div>
     )
   }
