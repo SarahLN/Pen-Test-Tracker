9c21a3760976decfed09383cea20c10bfd6faf87
SarahLN <noles.sarah@gmail.com>
2018-10-28 10:59:21 -0500

    persist user settings using local json file

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -3,13 +3,26 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import 'path';
+import UserSettingsStorage from '../utilities/user_setting_storage';
 
 class Settings extends React.Component {
   constructor(props) {
     super(props);
+
+    this.userSettings = new UserSettingsStorage({
+      configName: 'user-preferences',
+      defaults: {
+        data_folder: 'testpath'
+      }
+    });
+
+    console.log(this.userSettings);
+
     this.state = {
-      data_folder: '',
+      data_folder: this.userSettings.get('data_folder'),
     }
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -23,6 +36,7 @@ class Settings extends React.Component {
 
   handleSubmit(event) {
     alert('A data_folder was submitted: ' + this.state.data_folder);
+    this.userSettings.set('data_folder', this.state.data_folder);
     event.preventDefault();
   }
 
