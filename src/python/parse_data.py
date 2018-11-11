5ea6b087c5c055199a05676a1517e4c374f813e6
SarahLN <noles.sarah@gmail.com>
2018-11-10 19:13:38 -0600

    ad logic for parsing nmap file

--- src/python/parse_data.py
+++ src/python/parse_data.py
@@ -1,4 +1,21 @@
 import sys
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('ver_scan3.xml')
+root = tree.getroot()
+
+host = root[4]
+
+for element in host:
+    if element.tag == 'address':
+        # parse out the address
+        if element.attrib['addrtype'] == 'mac':
+            mac_addr = element.attrib['addr']
+        elif element.attrib['addrtype'] == 'ipv4':
+            ip_addr = element.attrib['addr']
+    elif element.tag == 'hostnames':
+        # parse out the hostname
+
 
 print('Hello from Python!')
 sys.stdout.flush()
