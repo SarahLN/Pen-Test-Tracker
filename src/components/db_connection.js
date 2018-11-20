f855c83909b23b5d1d4dc5ebb17744b82bc6a682
SarahLN <noles.sarah@gmail.com>
2018-11-19 20:04:12 -0600

    rewrite db_connection as a class and move to components folder

---
+++ src/components/db_connection.js
@@ -0,0 +1,31 @@
+class DB_Connection {
+
+  constructor() {
+    var mysql = require('mysql');
+    this.connection = mysql.createConnection({
+      host  : 'localhost',
+      user  : 'root',
+      password  : 'PASSWORDHERE',
+      database  : 'pentestdb'
+    });
+  }
+
+  connect_to_db() {
+    this.connection.connect();
+  }
+
+  disconnect_from_db() {
+    this.connection.end();
+  }
+
+  run_query(query) {
+    this.connect_to_db();
+    this.connection.query(query, function (error, results, fields) {
+      if (error) throw error;
+      console.log('query ran: ', query);
+    });
+    this.disconnect_from_db();
+  }
+};
+
+module.exports = DB_Connection;
