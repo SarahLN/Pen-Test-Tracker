c696e7b4ab0394dbb910eeb6440d00a302b17896
SarahLN <noles.sarah@gmail.com>
2018-11-19 21:13:04 -0600

    add functions to handle the results and assign them to state

--- src/components/filteritem.jsx
+++ src/components/filteritem.jsx
@@ -7,13 +7,30 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Checkbox from '@material-ui/core/Checkbox';
+import DB_Connection from './db_connection';
 
 class FilterItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      port_data: 0
+    };
+    this.get_data = this.get_data.bind(this);
+    this.save_data = this.save_data.bind(this);
+  };
+
+  save_data(results) {
+    this.setState({ port_data: results });
   }
 
+  get_data() {
+    var result = null;
+    let conn = new DB_Connection();
+    return conn.run_query('SELECT * FROM pentestdb.port;', this.save_data);
+  };
+
   render() {
+    var test = this.get_data();
     return (
       <div>
         <FormControl component="fieldset" className="filteritem">
