292df15447d5fdd6cb48ec79523b2d65b525174b
SarahLN <noles.sarah@gmail.com>
2018-11-25 19:02:59 -0600

    remove FilterItem and SearchBar references
    these are now handled by the MUI datatable

--- src/components/searchpage.jsx
+++ src/components/searchpage.jsx
@@ -5,8 +5,6 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
-import SearchBar from './searchbar'
-import FilterItem from './filteritem'
 import ResultsGrid from './resultsgrid'
 
 class SearchPage extends React.Component {
@@ -18,16 +16,6 @@ class SearchPage extends React.Component {
     return (
       <div>
         <Grid container spacing={16}>
-          <Grid item sm={12}>
-            <Paper elevation={1}>
-              <SearchBar />
-            </Paper>
-          </Grid>
-          <Grid item sm={2}>
-            <Paper elevation={1}>
-              <FilterItem name="Port" />
-            </Paper>
-          </Grid>
           <Grid item sm={10}>
             <Paper elevation={1}>
               <ResultsGrid />
